e313a7e00e1d12dfa2d7bd8f829c8a6638bc365a
AnayochukwuJames <anayochukwujames11@gmail.com>
2023-09-10 13:43:35 +0100

    first commit

--- src/main/java/com/example/emailverificationdemo/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.example.emailverificationdemo.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/emailverificationdemo/security/UsernameNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.emailverificationdemo.security;
-
-public class UsernameNotFoundException extends {
-    public UsernameNotFoundException(String userNotFound) {
-
-    }
-}
